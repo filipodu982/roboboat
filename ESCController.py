64add5450f6b4ea565c790ce261063dac8e4f2da
filipodu982 <filipodu982@gmail.com>
2024-07-29 19:20:33 +0200

    Initial commit, basic implementation and UTs

---
+++ ESCController.py
@@ -0,0 +1,28 @@
+import sys
+import pigpio
+
+
+class ESCController:
+    MIN_WIDTH = 1000  # Pulse width in us
+    MAX_WIDTH = 2000  # Pulse width in us
+    PULSE_RANGE = MAX_WIDTH - MIN_WIDTH
+
+    def __init__(self, pin_number):
+        self.pi = pigpio.pi()  # Instance of pigpio class
+        self.pin_number = pin_number  # Number of GPIO pin handling a motor
+
+        if not self.pi.connected:
+            sys.exit()
+
+    def change_motor_speed(self, speed_percent):
+        """Method which converts % of desired motor speed, to pulse width in us"""
+        if speed_percent > 100:
+            speed_percent = 100
+        elif speed_percent < 0:
+            speed_percent = 0
+
+        desired_pulse_width = (speed_percent / 100) * self.PULSE_RANGE + self.MIN_WIDTH
+        self.pi.set_servo_pulsewidth(self.pin_number, desired_pulse_width)
+
+    def __del__(self):
+        self.pi.set_servo_pulsewidth(self.pin_number, 0)
---
+++ test_esccontroller.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest import mock
+from ESCController import ESCController
+
+
+class testController(unittest.TestCase):
+    @mock.patch("ESCController.sys")
+    @mock.patch("ESCController.pigpio")
+    def test_initialization_of_class(self, mock_pi, mock_sys):
+
+        E = ESCController(4)
+
+        self.assertEqual(E.pin_number, 4)
+        mock_pi.pi.assert_called_once()
+        mock_sys.exit.assert_not_called()
+
+    @mock.patch("ESCController.pigpio")
+    def test_change_motor_speed(self, mock_pi):
+        mock_api = mock_pi.pi.return_value
+        mock_api.set_servo_pulsewidth = mock.Mock()
+
+        E = ESCController(4)
+
+        E.change_motor_speed(100)
+
+        E.change_motor_speed(0)
+
+        E.change_motor_speed(150)
+
+        calls = [mock.call(4, 2000), mock.call(4, 1000), mock.call(4, 2000)]
+        mock_api.set_servo_pulsewidth.assert_has_calls(calls)
+
+    @mock.patch("ESCController.pigpio")
+    def test_destructor(self, mock_pi):
+        mock_api = mock_pi.pi.return_value
+        mock_api.set_servo_pulsewidth = mock.Mock()
+
+        E = ESCController(4)
+
+        del E
+        mock_api.set_servo_pulsewidth.assert_called_once_with(4, 0)
